85d0cf97ae77a61a8db2627f83d7a017a32f5b76
xuyongqi <2844987572@qq.com>
2023-06-12 21:32:11 +0800

    修改恒源云linux不支持source的问题

--- xcmd.sh
+++ xcmd.sh
@@ -10,6 +10,11 @@ fi
 
 printf "### 运行命令："
 
+show_xcmd(){
+   grep -r "^#@xcmd" xcmd.sh | sed "s/#@xcmd //g" | sed "s/ /\t/1" | awk '{ printf "sh xcmd.sh %-20s\t%-20s\n",$1,$2}' | cat -n
+  return 0
+}
+
 #@xcmd add 执行git add操作
 add_list='Analysis README.md model requirements.txt run script conf lib xcmd.sh'
 if [ "$process_command" = "add" ]; then
@@ -32,8 +37,8 @@ elif [ "$process_command" = "clear_program" ]; then
   for file in $remove_files; do
     fileCount=$(find . '(' -name "$file" -path ./drops ')' | wc -l)
     echo "找到 $file 文件数量: $fileCount"
-    find . -name $file -exec git rm -r {} \;
-    find . -name $file -exec rf -r {} \;
+    find . -name $file -exec git rm -r --cache {} \;
+    find . -name $file -exec rm -r  {} \;
   done
 
 #@xcmd move_result 将result文件夹保存到oss
