a9f5904240b6d23d35a1b49d068456356e01ee25
xuyongqi <2844987572@qq.com>
2023-06-12 22:25:10 +0800

    增加了恒源云环境初始化脚本

---
+++ script/bash/env_init.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ ! -d "/hy-tmp" ]; then
+  echo "\033[31m不是恒源云环境\033[0m"
+  exit 1
+fi
+
+oss ls
+if [ $? -ne 0 ]; then
+  echo "\033[31m oss未登录 \033[0m"
+  exit 1
+fi
+
+if [ ! -d /home/dataset ]; then
+  mkdir -p /home/dataset && cd /home/dataset
+else
+  cd /home/dataset
+fi
+
+oss cp oss://datasets/v1_WMS_Simple_B.zip  .
+unzip oss://datasets/v1_WMS_Simple_B.zip &
+oss cp oss://datasets/v2_WMS_Sim_B.zip  .
+unzip oss://datasets/v2_WMS_Sim_B.zip
+
+cd /home && git clone https://github.com/jaffx/flow_identification.git
+
+exit 0
