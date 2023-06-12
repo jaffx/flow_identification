4cef5b8c935f5629f0792c0bb4e7139f251273fa
xuyongqi <2844987572@qq.com>
2023-06-12 21:59:36 +0800

    修改文件夹位置

--- Analysis/Analyzer.py
+++ Analysis/Analyzer.py
@@ -1,50 +1,23 @@
 import os
+from matplotlib import pyplot as plt
 
 
-class Analyzer():
-    def __init__(self, path):
-        self.path = path
+class Analyzer:
+    def __init__(self):
+        pass
 
-    def Analyse_train_iter(self, attrs=[]):
-        """
-        file_format:
-        ['Method', ' epoch', ' batch', 'NSample', 'AccNum', '   ACC', '  Loss', 'AVGLoss\n']
-        ['     T', '     0', '     1', '    32', '    10', '0.3125', '  1.39', '0.0433\n']
-        ['     T', '     0', '     2', '    32', '     9', '0.2812', '  1.37', '0.0428\n']
-        """
-        file_path = os.path.join(self.path, "train_iter")
-        with open(file_path) as fp:
-            content = fp.readlines()
-            results = {
-                "method": [],
-                "epoch": [],
-                "batch": [],
-                "sample_num": [],
-                "acc_num": [],
-                "acc": [],
-                "loss": [],
-                "avg_loss": [],
-            }
-            for line in content[1:]:
-                line = line[:-1]
-                datas = line.split('\t')
-                method = datas[0].strip()
-                epoch = int(datas[1])
-                batch = int(datas[2])
-                sample_num = int(datas[3])
-                acc_num = int(datas[4])
-                acc = float(datas[5])
-                loss = float(datas[6])
-                avg_loss = float(datas[7])
-                results["method"].append(method)
-                results["epoch"].append(epoch)
-                results["batch"].append(batch)
-                results["sample_num"].append(sample_num)
-                results["acc_num"].append(acc_num)
-                results["acc"].append(acc)
-                results["loss"].append(loss)
-                results["avg_loss"].append(avg_loss)
-            ret = {}
-            for attr in attrs:
-                ret[attr] = results[attr]
-            return ret
+    def readDataFromFile(self, path, idx):
+        assert os.path.isfile(path), f"{path}文件不存在"
+        datas = []
+        with open(path) as fp:
+            lines = fp.readlines()
+            for line in lines:
+                line = line.strip("\n")
+                items = line.split("\t")
+                assert len(items) >= idx, f"数据字段数量错误"
+                data = float(items[idx])
+                datas.append(datas)
+            fp.close()
+        return datas
+
+    def
