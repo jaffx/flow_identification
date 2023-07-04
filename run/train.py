88db6aa66888d147e0a205242dca2f876664e35c
xuyongqi <2844987572@qq.com>
2023-07-05 00:06:59 +0800

    fix

--- run/train.py
+++ run/train.py
@@ -26,7 +26,7 @@ def dealArgs():
     parser.add_argument('-s', '--step', type=int, default=2048, help='Step Length')
     parser.add_argument('-t', '--transform', type=str, default="normalization", help='Transform for train method')
     parser.add_argument('--lr', type=float, default=0.00001, help='learn rate')
-    parser.add_argument('--mod', type=str, default="test", help='epoch modifier')
+    parser.add_argument('--mod', type=str, default="None", help='epoch modifier')
 
     # 从命令行中结构化解析参数
     args = parser.parse_args()
